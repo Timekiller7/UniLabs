718457e42c021edb3c975687130e88e829848eda
Nastya <nastisha.m@gmail.com>
2021-09-12 19:23:45 +0300

    done

--- OOP/2/main.cpp
+++ OOP/2/main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include "one.h"
 #include "two.h"
+#include <cstring>
+using std::string;
 //34
 /*
  * float f char* ps
  * string s COne* p
  */
 int main() {
+    string temp = "working";
+
+    std::cout << "TEST FOR FIRST CLASS: " << std::endl;
     One ob(7,"f");
-    One ob2;
-    std::cout << "get char: " << ob.getPs() << std::endl;
+
+    One ocopy(ob);        //конструктор копирования
+    ocopy.print();
+    std::cout  << std::endl;
+
+    One ob2;               //проверка геттеров и тождества
+    std::cout << "get char of first object: " << ob.getPs() << std::endl;
+    std::cout  << std::endl;
+
     ob2.print();
     ob = ob2;
     ob.print();
 
+    std::cout  << std::endl << "TEST FOR SECOND CLASS: " << std::endl;
+    Two ob3(&ob, temp);
+    Two ob4;
+    Two copy(ob3);
+    copy.print();
+    std::cout  << std::endl;
+
+    //по умолчанию в конструкторе One: float = 1,5, char ="t"
+    copy = ob4;
+    copy.print();
+    std::cout  << std::endl;
+
+    copy.getObject();
 
     return 0;
 }
